cf2a66ca8f83f020803b47b933a4b561d7214e66
izzulamirr <izzulamir1602@gmail.com>
2026-04-16 15:27:05 +0800

    admin pages

--- helpdesk-web/src/lib/firebase.ts
+++ helpdesk-web/src/lib/firebase.ts
@@ -4,16 +4,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBIcKHdCrqh3LHPJ99nPFRmb29T3cPHcNU",
+  authDomain: "klsbhelpdesk.firebaseapp.com",
+  projectId: "klsbhelpdesk",
+  storageBucket: "klsbhelpdesk.firebasestorage.app",
+  messagingSenderId: "828464230591",
+  appId: "1:828464230591:web:74dcea73b0902ef7dd560a",
+  measurementId: "G-XZWV6X8PSN",
 };
 
-export const firebaseReady = Object.values(firebaseConfig).every(Boolean);
+export const firebaseReady = true;
 
 const app = firebaseReady
   ? getApps().length > 0
